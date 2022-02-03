579d2f43e15e9e18aad6669c5a83716294286778
provideforme <evan.a.bernard@gmail.com>
2022-02-03 17:00:16 -0500

    begin lab

--- js/app.js
+++ js/app.js
@@ -2,11 +2,30 @@
 
 
 
+
 /*---------------------------- Variables (state) ----------------------------*/
 
+// 1) Define the required variables used to track the state of the game:
+  // None of these variables will need to hold a value when they are defined
 
+	// 1.1) Use an array to represent the squares on the board. 
+let squares     
 
+	// 1.2) Use a turn variable to track whose turn it is.
+let turn
+	// 1.3) Use a winner variable to represent three different game states:
+	  // a player that won
+	  // a tie has occured
+	  // or a game that is still in play.
+let isWinner
 /*------------------------ Cached Element References ------------------------*/
+// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
+const allSquares = document.querySelector('.square')
+	
+	// 2.1) Store the 9 elements that represent the squares on the page.
+	  // You may want to give each square a class name in your HTML to make this easier!
+
+	// 2.2) Store the element that displays the game status on the page.
 
 
 
